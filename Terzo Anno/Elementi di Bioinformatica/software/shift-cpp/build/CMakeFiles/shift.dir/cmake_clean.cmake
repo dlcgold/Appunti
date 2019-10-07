fb9d1465983cadd070f056b10cc7c158ae5f3db5
dlcgold <dlcgold96@gmail.com>
2019-10-07 14:33:22 +0200

    fix dir bio

---
+++ Terzo Anno/Elementi di Bioinformatica/software/shift-cpp/build/CMakeFiles/shift.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/shift"
+  "../bin/shift.pdb"
+  "CMakeFiles/shift.dir/shift.cpp.o"
+  "CMakeFiles/shift.dir/shiftMethod.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shift.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

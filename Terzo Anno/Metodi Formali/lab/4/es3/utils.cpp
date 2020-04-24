f0f219dd1f5042c1c72b62c133f318a1feb40921
dlcgold <dlcgold96@gmail.com>
2020-04-24 10:46:38 +0200

    fix

--- Terzo Anno/Metodi Formali/lab/4/es3/utils.cpp
+++ Terzo Anno/Metodi Formali/lab/4/es3/utils.cpp
@@ -3,9 +3,10 @@
 
 std::string mToStr(std::vector<int> m){
   std::string result = "";
-  for(auto elem: m)
-    result += (std::to_string(elem) + " ");
-  result += "\b";
+  result += std::to_string(m[0]);
+  for(unsigned int i = 1; i < m.size(); i++)
+    result += (" " + std::to_string(m[i]));
+  //result += "\b";
   return result;
 }
 
